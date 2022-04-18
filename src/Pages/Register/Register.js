b2a314080db612111c0e9e993b48d27f7865bb50
unknown <you@example.com>
2022-04-19 03:09:23 +0600

    added login & register

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import './Register.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+      
+
     const navigate=useNavigate();
     const navigateLogin=event=>{
         
        navigate('/login') 
     }
+    if(user){
+        navigate('/home')
+    }
+    
     const handleRegister=event=>{
         event.preventDefault();
-        console.log(event.target);
+        const name=event.target.name.value;
+        const email=event.target.email.value;
+        const password=event.target.password.value;
+        createUserWithEmailAndPassword(email, password)
     }
     return (
         <div className='register-form'>
             <h2 className='text-center fw-bold'>Please Register</h2>
-            <Form>
+            <Form onSubmit={handleRegister}>
                 <input type="text" name="name" id="" placeholder='Your Name' />
                 <input type="email" name="email" id="" placeholder='Email Address' required />
                 <br />
