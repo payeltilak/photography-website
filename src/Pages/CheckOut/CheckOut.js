516864f16157301c845ef850a2665f0b46916fde
unknown <you@example.com>
2022-04-19 21:14:10 +0600

    check out added

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -6,7 +6,7 @@ const CheckOut = () => {
     return (
         <div>
             <img src="https://thumbs.dreamstime.com/z/secure-checkout-icon-21909253.jpg" alt="" />
-            <Button className='text-center btn-primary'>Check Out</Button>
+            <Button className='text-center btn-primary'>CheckOut</Button>
 
 
         </div>
